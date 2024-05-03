827b4f9e8ac9bee2963fd4f7f6b500008cb82cf4
gigomgigom <100766892+gigomgigom@users.noreply.github.com>
2024-05-03 16:23:54 +0900

    ProductDao interface added

--- src/main/java/com/mycompany/javajavajo/controller/AdminController.java
+++ src/main/java/com/mycompany/javajavajo/controller/AdminController.java
@@ -9,23 +9,32 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
-@Secured("ROLE_ADMIN")
+//@Secured("ROLE_ADMIN")
 @RequestMapping("/admin")
 public class AdminController {
+	
 	@RequestMapping("/main")
 	public String adminMain() {
 		return "admin/adminMain";
 	}
+	
 	@RequestMapping("/admin_dashboard")
 	public String adminDashBoard() {
 		return "admin/admin_dashboard";
 	}
+	
 	@GetMapping("/admin_member_view")
 	public String adminMemberView() {
 		return "admin/admin_member";
 	}
-	@GetMapping("/member_detail")
+	
+	@RequestMapping("/member_detail")
 	public String memberDetail() {
 		return "admin/admin_member_detail";
+	}
+	
+	@GetMapping("/add_product")
+	public String addProduct() {
+		return "admin/admin_add_product";
 	}
 }
